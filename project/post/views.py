35b3004fec1d413b1a947a1d6b414df7a7b18cbe
HamidrezaRzp <hamidreza.bigred3@gmail.com>
2025-06-11 15:33:57 +0330

    new permission added

--- project/post/views.py
+++ project/post/views.py
@@ -1,12 +1,15 @@
 from rest_framework import generics
 from .models import Post
 from .serializers import PostSerializer
+from .permissions import IsAuthorOrReadOnly
 
 class ListPosts(generics.ListCreateAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 class DetailPost(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     
